ed3b6291dd30817dc66f2790d89338c410ab7aa0
df1gg <dffgee4@gmail.com>
2026-08-16 23:49:02 +0300

    feat: test sys socket function to get IP addresses with domain name

---
+++ main.c
@@ -0,0 +1,48 @@
+#define _POSIX_C_SOURCE 200112L
+
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+
+int main(int argc, char *argv[]) {
+  struct addrinfo hints, *res, *p;
+  int status;
+  char ipstr[INET6_ADDRSTRLEN];
+
+  if (argc != 2) {
+    printf("\nUsage: %s <domain name>\n", argv[0]);
+    return 0;
+  }
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+    return 2;
+  }
+
+  for (p = res; p != NULL; p = p->ai_next) {
+    void *addr;
+    char *ipver;
+
+    if (p->ai_family == AF_INET) {
+      struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+      addr = &(ipv4->sin_addr);
+      ipver = "IPv4";
+    } else {
+      struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+      addr = &(ipv6->sin6_addr);
+      ipver = "IPv6";
+    }
+
+    inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
+    printf("> %s: [%s] %s\n", argv[1], ipver, ipstr);
+  }
+  freeaddrinfo(res);
+
+  return 0;
+}
