a80095fa5326588be9593b7d9771711ed10064f7
df1gg <dffgee4@gmail.com>
2026-08-29 23:05:48 +0300

    feat: responce dns function in dns server mock

--- tests/mock_server.py
+++ tests/mock_server.py
@@ -1,8 +1,9 @@
 import argparse
 import socket
+import struct
 
 
-def run_mock_server(port: int, mode: str):
+def run_mock_server(port: int, mode: str) -> None:
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("127.0.0.1", port))
     print(f"[MOCK] Server running on 127.0.0.1:{port} with {mode} mode...")
@@ -32,9 +33,16 @@ def run_mock_server(port: int, mode: str):
             sock.sendto(responce, addr)
             continue
 
-        responce = bytearray(data)
-        responce[2] |= 0x80  # QR bit = 1 (Responce)
+        responce = create_dns_responce(data)
         sock.sendto(responce, addr)
+
+def create_dns_responce(data: bytes, ip_address: str = "1.2.3.4") -> bytes:
+    tx_id = data[:2]
+    header = tx_id + struct.pack("!HHHHH", 0x8180, 1, 1, 0, 0)
+    question = data[12:]
+    answer_meta = struct.pack("!HHHIH", 0xC00C, 1, 1, 60, 4)
+    ip_bytes = socket.inet_aton(ip_address)
+    return header + question + answer_meta + ip_bytes
 
 
 if __name__ == "__main__":
